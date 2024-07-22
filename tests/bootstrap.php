cd0936fe24fbae7c877c775950537709dac35ae9
Maulikkumar Panchal <maulik882@gmail.com>
2024-07-22 12:44:38 +0530

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,7 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-    require dirname( __FILE__ ) . '/../dynamic-featured-image.php';
+    require_once dirname( __FILE__ ) . '/../dynamic-featured-image.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
